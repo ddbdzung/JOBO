0b4f5b4016e0b09678e143cab5029e93b1defd6f
kynhongg <hoangkyphongln@gmail.com>
2022-06-05 00:20:48 +0700

    [1.0.0][phong] tao giao dien dang nhap, dang ky

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import CreateAcc from "../components/CreateAcc";
+import Signin from "../components/Signin";
+
+const Home = () => {
+  return (
+    <div>
+      {/* <Signin /> */}
+      <CreateAcc />
+    </div>
+  );
+};
+
+export default Home;
