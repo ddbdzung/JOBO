bf1317b8ee4a0d42a82d596ad35670b983e7ea0c
hung279 <hungnher279@gmail.com>
2022-06-10 02:10:29 +0700

    [1.1.1][Hoonn]add API auth

--- server/src/app.js
+++ server/src/app.js
@@ -68,7 +68,7 @@ app.use('/api/v1', routes);
 
 // send back a 404 error for any unknown api request
 app.use((req, res, next) => {
-  next(new ApiError(httpStatus.NOT_FOUND, `Not found`));  
+  next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
 });
 
 // convert error to ApiError, if needed
